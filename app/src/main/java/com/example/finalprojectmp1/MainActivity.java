77ef2926edabcb12c5dc50c131f52f99d16699a6
mchittimelli <cmahesh804@gmail.com>
2019-07-10 17:18:06 -0400

    splash screen with logo and title empty home screen

--- app/src/main/java/com/example/finalprojectmp1/MainActivity.java
+++ app/src/main/java/com/example/finalprojectmp1/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.finalprojectmp1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.animation.ObjectAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Handler handler;
+    ImageView img,title;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img=findViewById(R.id.mainlogo);
+        title=findViewById(R.id.logo);
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(img,"rotationY",0.0f,360.0f);
+        animator.setDuration(5000);
+        animator.setRepeatCount(Animation.INFINITE);
+        animator.start();
+
+        Animation animFadeIn = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.fade_in);
+        title.startAnimation(animFadeIn);
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(MainActivity.this,HomeScreen.class);
+                startActivity(intent);
+                finish();
+            }
+        },5000);
+
     }
 }
