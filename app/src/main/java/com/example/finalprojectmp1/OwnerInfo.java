5fafaada949a58d943ba4770db2b5ef8e2002940
mchittimelli <cmahesh804@gmail.com>
2019-07-12 20:10:20 -0400

    fetching data for repo owner -- followers, following, organization

--- app/src/main/java/com/example/finalprojectmp1/OwnerInfo.java
+++ app/src/main/java/com/example/finalprojectmp1/OwnerInfo.java
@@ -8,10 +8,36 @@ import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
+
 public class OwnerInfo extends AppCompatActivity {
     ImageView owner_img;
     TextView ownerName,followers,following,organization,repos;
+    String followersUrl,followingUrl,organizationsUrl,reposUrl;
 
+
+public String nameList(String jsonURl,String property){
+    String result="";
+    try {
+        String  myjson = new syncdata().execute(jsonURl).get();
+        JSONArray myJsonArray= new JSONArray(myjson);
+        for(int x=0;x<myJsonArray.length();x++){
+            JSONObject childObj = myJsonArray.getJSONObject(x);
+            result=result+childObj.getString(property)+"\n";
+        }
+    } catch (ExecutionException e) {
+        e.printStackTrace();
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    } catch (JSONException e) {
+        e.printStackTrace();
+    }
+    return result;
+}
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,14 +48,28 @@ public class OwnerInfo extends AppCompatActivity {
         following=findViewById(R.id.following);
         organization=findViewById(R.id.organization);
         repos=findViewById(R.id.repos);
+        OwnerInfo ownerInfo=new OwnerInfo();
         Bundle bundle = getIntent().getExtras();
         String imgurl = bundle.getString("owner_img");
+
         ownerName.setText(bundle.getString("ownerName"));
-        followers.setText(bundle.getString("followers"));
-        following.setText(bundle.getString("following"));
-        organization.setText(bundle.getString("organization"));
+
+        followersUrl=bundle.getString("followers");
+        followingUrl=bundle.getString("following");
+        organizationsUrl=bundle.getString("organization");
+
         repos.setText(bundle.getString("repos"));
+        reposUrl=bundle.getString("repos");
+
         Picasso.get().load(imgurl).into(owner_img);
+
+
+        //for followers
+       followers.setText(ownerInfo.nameList(followersUrl,"login"));
+       //for following
+        following.setText("Data not found");
+         //for organizations
+        organization.setText(ownerInfo.nameList(organizationsUrl,"login"));
 
     }
 }
